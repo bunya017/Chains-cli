4b658d761b21f6a57992c70d1693f91d81384c43
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-07 06:15:37 +0100

    Created about component

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about container">
+    <h1 class="pt-2">ABOUT</h1>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data: function() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
