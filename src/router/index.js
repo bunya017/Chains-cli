29d3cf751377990af445f134e5e55a7c57182261
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-07 06:25:52 +0100

    Created route for Habit, HabitDetail, About, Add & Login components

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Habits from '@/components/Habits'
+import About from '@/components/About'
+import Login from '@/components/Login'
+import Add from '@/components/Add'
+import HabitDetail from '@/components/HabitDetail'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path: '/habits', name: 'habits', component: Habits},
+    {path: '/habits/:id', name: 'habitDetail', component: HabitDetail},
+    {path: '/about', name: 'about', component: About},
+    {path: '/login', name: 'login', component: Login},
+    {path: '/add', name: 'add', component: Add},
+  ]
+})
