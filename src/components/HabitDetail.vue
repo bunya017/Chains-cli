c6895efa1df427317f9b64009e54c773572d1933
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-07 06:16:30 +0100

    Created HabitDetail component for the habit-detail endpoint

---
+++ src/components/HabitDetail.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="habitdetail container">
+    <div class="card card-body col-md-6 mx-auto my-2 border-dark">
+      <h1 class="py-2">{{ habit.name }}</h1>
+      <p>Goal: <span class="ml-3">{{ habit.goal }}</span></p>
+      <p>Start on: <span class="ml-3">{{ habit.start_date }}</span></p>
+      <p>End on: <span class="ml-3">{{ habit.stop_date }}</span></p>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Weeks</th>
+            <th colspan="7">Entries</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>22-Oct-2018</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>29-Oct-2018</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'habitdetail',
+  data: function() {
+    return {
+      habit: {},
+      authToken: this.getCookie('brokenChainsAuthToken'),
+    }
+  },
+  methods: {
+    getCookie: function(cookieName) {
+      var name = cookieName + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0; i<ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1);
+          if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+      }
+      return "";
+    },
+    fetchHabit: function(id) {
+      this.$http.get('http://127.0.0.1:8000/api/habits/'+id, {headers: {'Authorization': 'Token '+this.authToken,}})
+        .then(function(response){
+          this.habit = response.body;
+        });
+    },
+  },
+  created: function() {
+    this.fetchHabit(this.$route.params.id);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
