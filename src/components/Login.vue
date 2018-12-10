f0c26d762b8f10ade53ad5eaa401ea5139e5e255
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-10 13:47:37 +0100

    1. Improved login UI. 2. Catch invalid login credentials error.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,42 +1,58 @@
 <template>
   <div class="login container">
-    <h1 class="pt-2">Login</h1>
-    <hr>
-    <form v-on:submit.prevent="getAuthToken">
-      <div class="form-group">
-        <label>Username:</label>
-        <input type="text" class="form-control" placeholder="Enter username" v-model="user.username">
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" placeholder="Password" v-model="user.password">
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="col-md-6 mx-auto card card-body mt-3">
+      <h1 class="pb-3 text-center">Login</h1>
+      <Alert v-if="alert" v-bind:message="alert" v-bind:class="'alert-danger'" />
+      <form v-on:submit.prevent="getAuthToken">
+        <div class="form-group">
+          <label>Username:</label>
+          <input type="text" class="form-control" placeholder="Enter username" v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label>Password:</label>
+          <input type="password" class="form-control" placeholder="Password" v-model="user.password">
+        </div>
+        <button type="submit" class="btn btn-primary btn-lg">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import Alert from './Alert'
 export default {
   name: 'login',
   data: function() {
     return {
       user: {},
+      alert: '',
     }
+  },
+  components: {
+    Alert
   },
   methods: {
     getAuthToken: function() {
       let loginUser = {
         username: this.user.username,
-        password: this.user.password
+        password: this.user.password,
       }
       this.$http.post('api-token-auth/', loginUser)
-        .then(function(response){
-          document.cookie = "brokenChainsAuthToken=" + response.body.token +";";
-      });
+        .then(
+          (response) => {
+            document.cookie = "brokenChainsAuthToken=" + response.body.token;
+          },
+          (error) => {
+            this.$router.push({path: '/login', query: {alert: error.body.non_field_errors[0]}})
+          }
+      );
     },
   },
-
+  created: function() {
+    if(this.$route.query.alert){
+      this.alert = this.$route.query.alert;
+    }
+  },
 }
 </script>
 
