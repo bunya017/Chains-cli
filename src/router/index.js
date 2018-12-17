e4720fa9b0df02988730da7a7fe97e2a9dfaeb1a
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-17 22:11:37 +0100

    added navigation guard for login

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,82 @@ import Login from '@/components/Login'
 import Add from '@/components/Add'
 import Signup from '@/components/Signup'
 import HabitDetail from '@/components/HabitDetail'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: __dirname,
   routes: [
-    {path: '/habits', name: 'habits', component: Habits},
-    {path: '/habits/:id', name: 'habitDetail', component: HabitDetail},
-    {path: '/about', name: 'about', component: About},
-    {path: '/login', name: 'login', component: Login},
-    {path: '/add', name: 'add', component: Add},
-    {path: '/signup', name: 'signup', component: Signup},
+    {
+      path: '/habits',
+      name: 'habits',
+      component: Habits,
+      meta: {
+        requiresAuth: true,
+      }
+    },
+    {
+      path: '/habits/:id',
+      name: 'habitDetail',
+      component: HabitDetail,
+      meta: {
+        requiresAuth: true,
+      }
+    },
+    {
+      path: '/add',
+      name: 'addHabit',
+      component: Add,
+      meta: {
+        requiresAuth: true,
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+      meta: {
+        requiresAuth: false,
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresAuth: false,
+      }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+      meta: {
+        requiresAuth: false,
+      }
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Habits,
+      meta: {
+        requiresAuth: true,
+      }
+    },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let userAuthenticated = function() {
+    return JSON.parse(localStorage.getItem('isLoggedIn'))
+  }
+  if (to.meta.requiresAuth && !userAuthenticated()) {
+    next('/login');
+  } else {
+    next();
+    console.log(!userAuthenticated())
+  }
+})
+
+export default router
