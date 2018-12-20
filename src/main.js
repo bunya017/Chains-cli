9d5fdb2eda90678153a16e42cf9c5ccdde2cf93b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2018-12-20 20:29:58 +0100

    made nav-brand link to '/habits' instead of '/'

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ new Vue({
     <div id='app'>
       <nav class="navbar navbar-expand-md navbar-dark bg-info shadow">
         <div class="container">
-          <router-link to="/" class="navbar-brand">brokenChains</router-link>
+          <router-link to="/habits" class="navbar-brand">brokenChains</router-link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
